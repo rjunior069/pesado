4550a97695d2c8740f386e779fc1318be62dc48d
rjunior069 <rjunior069@gmail.com>
2019-07-04 11:45:59 -0300

    atualizando nodeJS

--- JavaScriptCurse/app/node.js
+++ JavaScriptCurse/app/node.js
@@ -2,9 +2,28 @@ const express=require("express");
 
 const app = express();
 
+app.get("/", function(req, res){
+    res.send("Welcome to my app!");
+
+});
+
+app.get("/sobre", function(req, res){
+    res.send("Minha pagina Sobre");
+});
+
+app.get("/blog",function(req, res){
+    res.send("Welcome to my Blog");
+});
+
+app.get('/ola/:nome/:cargo/:cor',function(req, res){
+    res.send("<h1>Ola  "+req.params.nome+"<h1/> " + "<h2> Seu cargo e: "+req.params.cargo+"<h2/>" + "<h3> Sua cor e: "+req.params.cor+"<h3/>");
+//send sóm poder ser enviado uma vez por rota ou função
+});
+
+
 app.listen(8081, function(){
 
-    console.log("Servidor rodandona url informada htt:/localhost:8081");
+    console.log("Servidor rodandona url informada http:/localhost:8081");
 
 });
 
